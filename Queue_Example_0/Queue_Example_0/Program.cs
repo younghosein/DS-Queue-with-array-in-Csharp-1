bd53fad72ec756bd9d244145c7f980d1690117ad
Hosein Mohamadpor <83702864+younghosein@users.noreply.github.com>
2023-05-22 01:11:21 +0330

    Add files via upload

---
+++ Queue_Example_0/Queue_Example_0/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_Example_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue s = new Queue(10);
+
+            s.enqueue(21);
+            s.enqueue(92);
+            s.enqueue(61);
+            s.enqueue(88);
+            s.enqueue(43);
+            s.enqueue(75);
+            s.enqueue(59);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Queue Is : ");
+            s.print();
+            Console.ResetColor();
+            Console.WriteLine("\n");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            s.dequeue();
+            Console.WriteLine("Now Queue Is : ");
+            s.print();
+            Console.ResetColor();
+            Console.WriteLine("\n");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            int a = s.avg();
+            Console.WriteLine("Average Is : ");
+            Console.WriteLine(a);
+            Console.ResetColor();
+            Console.WriteLine("\n");
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Queue_Example_0/Queue_Example_0/Queue.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_Example_0
+{
+    class Queue
+    {
+        public int[] q;
+        public int front;
+        public int rear;
+        public int max;
+
+        public Queue(int size)
+        {
+            q = new int[size];
+            max = size;
+            rear = -1;
+            front = 0;
+        }
+        public void print()
+        {
+            if (front == rear + 1)
+            {
+                Console.WriteLine("Queue Is Empty !");
+                return;
+            }
+            else
+            {
+                for (int i = front; i <= rear; i++)
+                {
+                    Console.Write(q[i] + " . ");
+                }
+            }
+        }
+        public void enqueue(int data)
+        {
+            if (rear == max - 1)
+            {
+                Console.WriteLine("Queue Is Full !");
+                return;
+            }
+            q[++rear] = data;
+        }
+        public int dequeue()
+        {
+            if (rear == front - 1)
+            {
+                Console.WriteLine("Queue Is Empty !");
+                return -1;
+            }
+            Console.WriteLine($"{q[front]} Is Delete From Queue...");
+            return q[++front];
+        }
+         public int avg()
+        {
+            int sum = 0;
+            int c = 0;
+            int avg;
+            if (front == rear + 1)
+            {
+                Console.WriteLine("Queue Is Empty !");
+                return -1;
+            }
+            else
+            {
+                for (int i = front; i <= rear; i++)
+                {
+                    sum += q[i];
+                    c++;
+                }
+                Console.Write($"Sum Is : {sum}\n");
+                Console.Write($"Count Is : {c}\n");
+                avg = sum / c;
+                return avg;
+            }
+        }
+    }
+}
